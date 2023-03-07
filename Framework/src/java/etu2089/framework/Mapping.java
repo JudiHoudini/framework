eaac26780a71ddcaaae93ffd047e338b3bdd4073
judihoudini <melomam5678@gmail.com>
2023-03-07 14:00:06 +0300

    sprint 2

---
+++ Framework/src/java/etu2089/framework/Mapping.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package etu2089.framework;
+
+
+/**
+ *
+ * @author judi
+ */
+public class Mapping {
+    String className;
+    String method;
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+    
+    
+}
--- Framework/src/java/etu2089/framework/servlet/FrontServlet.java
+++ Framework/src/java/etu2089/framework/servlet/FrontServlet.java
@@ -2,10 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-package controleur;
+package etu2089.framework.servlet;
 
+import etu2089.framework.Mapping;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,31 +18,17 @@ import javax.servlet.http.HttpServletResponse;
  * @author judi
  */
 public class FrontServlet extends HttpServlet {
-    String page;
-    String methode;
+    HashMap<String, Mapping> mappingUrls;
 
-    public String getPage() {
-        return page;
-    }
-
-    public void setPage(String page) {
-        this.page = page;
+    public HashMap<String, Mapping> getMappingUrls() {
+        return mappingUrls;
     }
 
-    public String getMethode() {
-        return methode;
+    public void setMappingUrls(HashMap<String, Mapping> mappingUrls) {
+        this.mappingUrls = mappingUrls;
     }
-
-    public void setMethode(String methode) {
-        this.methode = methode;
-    }
-    public void setPageUrl(String url){
-        this.setPage(url.split("/")[1]);
-    }
-    public void setMethodeURL(String url){
-        if(url.split("/").length > 2)this.setMethode(url.split("/")[2]);
-        else this.setMethode(null);
-    }
+    
+    
     
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -64,10 +52,7 @@ public class FrontServlet extends HttpServlet {
             out.println("<body>");
             out.println("<h1>Servlet FrontServlet at " + request.getContextPath() + "</h1>");
             String path = request.getServletPath();
-            this.setPageUrl(path);
-            this.setMethodeURL(path);
-            out.print("page = " + this.getPage() + "<br>");
-            out.print("methode = " + this.getMethode());
+            out.print(path);
             out.println("</body>");
             out.println("</html>");
         }
